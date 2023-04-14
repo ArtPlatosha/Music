1019d80ae56f108cfa4d86754ccef06d04814164
ArtPlatosha <zipex98@gmail.com>
2023-04-14 08:37:29 +0300

    commit: 14th commit
    adding JS for gallery-section

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$ (document).ready(function() {
+
+	$('.button').click(function() {
+		$(this).addClass('active').siblings().removeClass('active');
+
+		var filter = $(this).attr('data-filter')
+
+		if(filter == 'all') {
+			$('.image').show(400);
+		}
+		else {
+			$('.image').not('.'+filter).hide(200);
+			$('.image').filter('.'+filter).show(200)
+		}
+	});
+
+	$('.gallery').magnificPopup({
+		delegate:'a',
+		type:'image',
+		gallery:{
+			enabled:true,
+		}
+	})
+});
